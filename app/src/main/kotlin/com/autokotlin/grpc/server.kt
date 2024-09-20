2b9090b6b8dc31123469c91ebbb7d7340d88227c
Bradley Thompson <bradlet2@pdx.edu>
2024-09-19 23:15:38 -0700

    Save progress on better server config api

--- app/src/main/kotlin/com/autokotlin/grpc/server.kt
+++ app/src/main/kotlin/com/autokotlin/grpc/server.kt
@@ -1,30 +1,69 @@
 package com.autokotlin.grpc
 
+import io.grpc.BindableService
 import io.grpc.ServerBuilder
+import io.grpc.health.v1.HealthCheckResponse
 import io.grpc.protobuf.services.ProtoReflectionService
+import io.grpc.protobuf.services.HealthStatusManager
 import kotlin.coroutines.EmptyCoroutineContext
 import mu.KotlinLogging
 
-private val logger = KotlinLogging.logger { }
+private val logger = KotlinLogging.logger(GrpcServer::class.java.simpleName)
 
 class GrpcServer(
-    private val port: Int = 6565,
-    private val devMode: Boolean = true,
-    serverConfiguration: ServerBuilder<*>.() -> Unit
+    configurator: GrpcServerConfiguration.() -> Unit
 ) {
-    private val server = ServerBuilder
-        .forPort(port)
-        .apply(serverConfiguration)
-        .apply {
-            if (devMode) {
-                addService(ProtoReflectionService.newInstance())
+    class GrpcServerConfiguration {
+        internal lateinit var healthStatusManager: HealthStatusManager
+        private val bindableServices = mutableListOf<BindableService>()
+
+        var port: Int = 6565
+        var enableHealthChecks: Boolean = true
+        var enableReflection: Boolean = true
+
+        fun addService(service: BindableService) {
+            bindableServices.add(service)
+        }
+
+        fun addServices(vararg services: BindableService) {
+            bindableServices.addAll(services)
+        }
+
+        internal fun build() = ServerBuilder
+            .forPort(port)
+            .apply {
+                if (enableReflection) {
+                    addService(ProtoReflectionService.newInstance())
+                }
+                if (enableHealthChecks) {
+                    healthStatusManager = HealthStatusManager()
+                    addServices(healthStatusManager.healthService)
+                }
+            }
+            .build()
+
+        internal fun signalServerUp() {
+            if (enableHealthChecks) {
+                bindableServices.forEach {
+                    healthStatusManager.setStatus(it.javaClass.simpleName, HealthCheckResponse.ServingStatus.SERVING)
+                }
             }
         }
-        .build()
+
+        internal fun signalServerDown() {
+            if (enableHealthChecks) {
+                bindableServices.forEach {
+                    healthStatusManager.setStatus(it.javaClass.simpleName, HealthCheckResponse.ServingStatus.NOT_SERVING)
+                }
+            }
+        }
+    }
+    private val configuration = GrpcServerConfiguration().apply(configurator)
+    private val server = configuration.build()
 
     fun start() {
         server.start()
-        println("Server started, listening on $port")
+        println("Server started, listening on ${configuration.port}")
         Runtime.getRuntime().addShutdownHook(
             Thread {
                 println("*** shutting down gRPC server since JVM is shutting down")
